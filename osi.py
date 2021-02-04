7793aea719d59c0c46cde9925507792a65f73a5b
UVvirus <39599249+UVvirus@users.noreply.github.com>
2021-02-04 18:16:16 +0530

    Update osi.py

--- osi.py
+++ osi.py
@@ -27,6 +27,34 @@ def clean_data():
     f1.close()
     f2.close()
 
+def check_acl():
+    print("inside acl")
+    PREDEFINED_URI=["http://acs.amazonaws.com/groups/global/AllUsers",
+    "http://acs.amazonaws.com/groups/global/AuthenticatedUsers"]
+    bucket='wordlist.txt'
+    list_of_buckets=[]
+    s3=boto3.resource('s3')
 
+    bucket_acl = s3.BucketAcl(bucket)
+    bucket_acl.load()
+
+
+    for granted in bucket_acl.grants:
+        if 'URI' in granted['Grantee']:
+
+            if granted["Grantee"]["URI"] == PREDEFINED_URI:
+                list_of_buckets.append(granted["Permission"])
+    return list_of_buckets
+
+def check_without_creds(bucket_url):
+    request=requests.head(bucket_url)
+    if request.status_code==200:
+        soup = BeautifulSoup(request.content, 'html.parser')
+        print(soup.prettify())
+   
+    
+    
 github(name='UVvirus')
 clean_data()
+check_acl()
+check_without_creds(bucket_url)
